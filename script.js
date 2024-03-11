1f60fed2be81b4b75c49d275655a2c7bb28440d7
Marek Sulikowski <mareksulikowski@gmail.com>
2024-03-11 21:58:50 +0100

    [writing code]: Closing the modal window with a cross sign

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ for (let i = 0; i < btnsOpenModal.length; i++)
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
   });
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
