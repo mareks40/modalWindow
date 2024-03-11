fd338281b52bb52e3db87c202e366d60131852ac
Marek Sulikowski <mareksulikowski@gmail.com>
2024-03-11 22:17:47 +0100

    [code refactoring]: openinig modal

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
+// Opening the modal window
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//Closing the modal window
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
