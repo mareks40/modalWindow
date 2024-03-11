2a81ceb015ce7cbb7690ca2d642c23ebcf2101ff
Marek Sulikowski <mareksulikowski@gmail.com>
2024-03-11 22:10:59 +0100

    [code refactoring]: close modal

--- script.js
+++ script.js
@@ -13,12 +13,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
     overlay.classList.remove('hidden');
   });
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
 
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
